9270e569e5bf6583479ad9867d49e259629c40b6
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2026-04-10 14:28:38 +0530

    Added Components and Continue setup
    added tawk.to to index.html and added logo and icon for public folder and created basic require pages.

---
+++ src/app/pages/home-page-context/home-page-context.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-context',
+  imports: [],
+  templateUrl: './home-page-context.html',
+  styleUrl: './home-page-context.scss',
+})
+export class HomePageContext {}
